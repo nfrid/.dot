8066869e467043491c81f0b856df2322852a75cc
Nick Friday <nfriday@yandex.ru>
2022-01-28 19:23:22 +0300

    add betterdiscord

---
+++ .config/BetterDiscord/js/fade_status.js
@@ -0,0 +1,11 @@
+return {
+    
+    const 
+ 
+        const fade = document.querySelector('div.headerPlaying-j0WQBV.header-2BwW8b.size16-14cGz5.da-headerPlaying.da-header.da-size16');
+    
+        const status = document.querySelector('div.avatarWrapperNormal-3wFMbf.avatarWrapper-3H_478.da-avatarWrapperNormal.da-avatarWrapper > div > svg > rect');
+    
+        const statusColor = status.getAttribute('fill');
+    
+}

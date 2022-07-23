838edb62baa6e34b259d4bdd0e7558075ce96c65
Nick Friday <nfriday@yandex.ru>
2022-07-24 01:22:17 +0300

    upd

--- .config/fish/keys.fish
+++ .config/fish/keys.fish
@@ -1,12 +1,12 @@
 bind -M insert \cn 'pager-toggle-search'
-bind -M normal \cn 'pager-toggle-search'
+bind -M default \cn 'pager-toggle-search'
 bind -M insert \cn 'complete-and-search'
-bind -M normal \cn 'complete-and-search'
+bind -M default \cn 'complete-and-search'
 
 bind -M insert \cj 'down-line'
 bind -M insert \ck 'up-line'
-bind -M normal \cj 'down-line'
-bind -M normal \ck 'up-line'
+bind -M default \cj 'down-line'
+bind -M default \ck 'up-line'
 
 bind -M insert \ca 'beginning-of-line'
 bind -M insert \ce 'end-of-line'
@@ -16,3 +16,8 @@ bind -M insert \eb 'backward-word'
 
 bind -M insert \cf 'forward-single-char'
 bind -M insert \cb 'backward-char'
+
+bind -M insert \e. '__fish_list_current_token'
+bind -M default \e. '__fish_list_current_token'
+bind -M insert \em '__fish_man_page'
+bind -M default \em '__fish_man_page'
